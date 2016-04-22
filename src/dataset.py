decec25b01679f77b533eab7ce570f4f8a5de201
adolenc <andrej.dolenc@student.uni-lj.si>
2016-04-22 14:50:56 +0200

    Fix Y in load_dataset to be array of ints

--- src/dataset.py
+++ src/dataset.py
@@ -5,7 +5,7 @@ from utils import flatten
 
 def load_dataset(directories=['data/tea_cup', 'data/spoon', 'data/apple']):
     """ Load classes from DIRECTORIES and generate X and Y matrices. """
-    images = map(load_all_images, directories)
-    X = flatten([to_vector(to_grayscale(clss)) for clss in images])
-    Y = flatten([[d] * len(clss) for d, clss in zip(directories, images)])
+    classes = map(load_all_images, directories)
+    X = flatten([to_vector(to_grayscale(clss)) for clss in classes])
+    Y = flatten([[l] * len(clss) for l, clss in enumerate(classes)])
     return np.array(X), np.array(Y)
